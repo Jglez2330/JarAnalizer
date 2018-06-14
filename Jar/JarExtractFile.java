00793178eb45f68b7598b23c4009f77b99a537e9
Java-Sith <prieto.luisdaniel@gmail.com>
2018-06-14 17:19:25 -0600

    Add files via upload
    Código que puede leer un jar y extraer las dependencias y clases dentro de ese jar en un arraylist

---
+++ Jar/JarExtractFile.java
@@ -0,0 +1,39 @@
+package Jar;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class JarExtractFile {
+	//Variables needed for code
+	static JarFile jar;
+	static List<String> list = new ArrayList<String>();
+	//Opens a jar file and gets all dependencies inside
+	public static void getJars(JarFile jar) throws Exception {
+		jar = new JarFile("Cualquier jar con dependencias");
+		Enumeration entries = jar.entries();
+		while(entries.hasMoreElements()) {
+			JarEntry jarentry = (JarEntry)entries.nextElement();
+			if(jarentry.getName().endsWith(".jar")) {
+				list.add(jarentry.getName());
+			}
+		}
+		System.out.println(list.toString());
+		jar.close();
+	}
+	//Opens a jar file and gets all classes inside
+	public static void getClasses(JarFile jar) throws Exception {
+		jar = new JarFile("Cualquier jar con clases");
+		Enumeration entries = jar.entries();
+		while(entries.hasMoreElements()) {
+			JarEntry jarentry = (JarEntry)entries.nextElement();
+			if(jarentry.getName().endsWith(".class")) {
+				list.add(jarentry.getName());
+			}
+		}
+		System.out.println(list.toString());
+		jar.close();
+	}
+}
