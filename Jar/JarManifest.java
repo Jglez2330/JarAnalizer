9b0418d7013436581c7dfde96cb231b7af8d62ff
Java-Sith <prieto.luisdaniel@gmail.com>
2018-06-18 08:29:41 -0600

    Add files via upload
    Se incluye el código de las dependencias

---
+++ Jar/JarManifest.java
@@ -0,0 +1,23 @@
+package Jar;
+
+import java.util.Iterator;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+public class JarManifest {
+	static JarFile jarfile;
+	public static void main (String[] args) throws Exception {
+		jarfile = new JarFile("C:\\Users\\Daniel\\eclipse\\Jar Files\\jackson-databind-2.9.5.jar");
+		getFromManifest(jarfile);
+	}
+	public static void getFromManifest(JarFile j) throws Exception {
+		Manifest manifest = j.getManifest();
+		Attributes attrs = manifest.getMainAttributes();
+	    for (Iterator it = attrs.keySet().iterator(); it.hasNext();) {
+	    	Attributes.Name attrName = (Attributes.Name) it.next();
+	    	String attrValue = attrs.getValue(attrName);
+	    	System.out.println(attrName + ": " + attrValue);
+	    }
+	}
+}
