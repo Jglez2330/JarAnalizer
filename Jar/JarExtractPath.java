1199fc96584cda186c65993eb362e67ebcdabb3e
Java-Sith <prieto.luisdaniel@gmail.com>
2018-06-18 22:25:42 -0600

    Add files via upload
    Extracción de dependencias de los Jars y lista que va al grafo

--- Jar/JarExtractPath.java
+++ Jar/JarExtractPath.java
@@ -3,19 +3,22 @@ package Jar;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.collections4.CollectionUtils;
 
 public class JarExtractPath {
 	//Variables
-	static List<String> listC;
-	static List<String> listD;
-	static List<String> listJ;
-	static List<String> listF;
+	static List<String> listC = new ArrayList<String>();
+	static List<String> listD = new ArrayList<String>();
+	static List<String> listJ = new ArrayList<String>();
+	static List<String> listF = new ArrayList<String>();
 	static File jar;
 	//Opens the classpath and obtains a list of all jars
 	public static void main (String args[]) throws Exception {
-		jar = new File("Aquï¿½ va el Jar");
+		jar = new File("C:\\Users\\Daniel\\Downloads\\commons-collections4-4.0.jar");
+		getClassPath();
+		System.out.println(listC.toString());
 	}
 	public static List<String> getClassPath() throws Exception {
 		String s = System.getProperty("java.class.path");
@@ -47,17 +50,24 @@ public class JarExtractPath {
 	public static List<String> validateJars (List<String> list) {
 		for(int i = 0; i < list.size(); i++) {
 			if(list.get(i).endsWith(".jar")) {
-				listgF.add(list.get(i));
+				listJ.add(list.get(i));
 			}
 		}
 		return listJ;
 	}
+	//Compares the classpath jars with dependencies jars
 	public static List<String> compareLists (List<String> list1, List<String> list2){
 		listF = (List<String>) CollectionUtils.retainAll(list1, list2);
 		if(listF == null) {
-			//Descargas de Maven van aquï¿½, mientras tanto
+			//Descargas de Maven van aquí, mientras tanto
 			return null;
 		}
 		return listF;
+	}
+	public static List<String> sendtoGraph(List<String> list) {
+		List<String> listOut = new ArrayList<String>();
+		listOut.add(jar.getPath());
+		listOut.addAll(list);
+		return listOut;
 	}
 }
--- Jar/JarManifest.java
+++ Jar/JarManifest.java
@@ -5,13 +5,20 @@ import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
+
 public class JarManifest {
 	static JarFile jarfile;
 	public static void main (String[] args) throws Exception {
-		jarfile = new JarFile("C:\\Users\\Daniel\\eclipse\\Jar Files\\jackson-databind-2.9.5.jar");
-		getFromManifest(jarfile);
+		jarfile = new JarFile("C:\\Users\\Daniel\\eclipse\\Jar Files\\jackson-core-2.9.5.jar");
+		getManifest(jarfile);
 	}
 	public static void getFromManifest(JarFile j) throws Exception {
+		Manifest manifest = j.getManifest();
+		Attributes attrs = manifest.getMainAttributes();
+		String s = attrs.getValue("Import-Package");
+		System.out.println(s);
+	}
+	public static void getManifest(JarFile j) throws Exception {
 		Manifest manifest = j.getManifest();
 		Attributes attrs = manifest.getMainAttributes();
 	    for (Iterator it = attrs.keySet().iterator(); it.hasNext();) {
